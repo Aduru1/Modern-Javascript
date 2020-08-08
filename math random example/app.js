03e704c35e1ac94ea5879e5d70a3625571f7fb93
Aduru1 <adururakeshkumar@gmail.com>
2020-08-08 21:29:21 +0530

    math random

---
+++ math random example/app.js
@@ -0,0 +1,7 @@
+//Print a random number from 0 -10 to console
+var num =Math.random(),
+    finalNum=num *10; //to get random number 0-10
+
+console.log(Math.round(finalNum));
+
+console.log(finalNum);
