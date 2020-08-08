961de2ebaa36ecabc249b127d2d2045e673e1c04
Aduru1 <adururakeshkumar@gmail.com>
2020-08-08 19:48:22 +0530

    declare multiple variables at top of code at once

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-var name = "Rakesh";
+var name = "Rakesh",
+    greeting = "hi",
+    punctuation = "!";
 
 
 test(); //calling function or execute the function
 
 console.log( greeting + name + punctuation);
 
-var greeting = "Hello there";
-
-
-var punctuation = "!";
-console.log( greeting + name + punctuation);
 
 function test(){
     console.log(greeting + name + punctuation);
