eeea46dcfd2e4e56929b1258c2f024868985ac22
Aduru1 <adururakeshkumar@gmail.com>
2020-08-08 21:13:46 +0530

    date format added folder

---
+++ date format/dateformat.js
@@ -0,0 +1,10 @@
+//create a date object
+
+var date =new Date(),
+    day=date.getDate(),
+    month=date.getMonth(),
+    year=date.getFullYear();
+
+//print date in format day/month/year
+month++;
+console.log(day + "/"+ month+ "/" +year);
